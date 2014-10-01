f700114aeda028ffff5894c2cbccad912e65bfe0
patronpenguin <xh5277@gmail.com>
2014-10-01 00:03:13 -0400

    Implementing HTML response format

---
+++ hw/instructor/src/edu/nyu/cs/cs2580/HtmlGenerator.java
@@ -0,0 +1,9 @@
+package edu.nyu.cs.cs2580;
+
+import java.util.Vector;
+
+public class HtmlGenerator {
+	public static String generateFromScoredDocuments(Vector<ScoredDocument> documents) {
+		return "stub";
+	}
+}
